d7ed9c9b933472b854557d57b637a039d565e0e8
MashaLar <mashalar01@gmail.com>
2021-03-01 23:11:50 +0200

    Update main class
    Added start of program to print chessboard
    with args from main class and catch exceptions

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -1,7 +1,18 @@
 package task1;
 
+import task1.model.Chessboard;
+
 public class Main {
     public static void main(String... args){
+
+        try{
+            int m = Integer.parseInt(args[0]);
+            int n = Integer.parseInt(args[1]);
+            Chessboard chessboard = new Chessboard(m,n);
+            chessboard.PrintDesk();
+        }catch (Exception e){
+            Chessboard.Instruction();
+        }
 
     }
 }
