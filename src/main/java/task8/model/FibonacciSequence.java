6528b25e8ccd62d26068e205fb1e28c5e05ebd0d
MashaLar <mashalar01@gmail.com>
2021-03-04 23:04:58 +0200

    Created main and model
    Created FibonacciSequence model
    Created empty main

---
+++ src/main/java/task8/model/FibonacciSequence.java
@@ -0,0 +1,47 @@
+package task8.model;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class FibonacciSequence {
+
+    //list for sequence
+    ArrayList<String> fibonacci = new ArrayList<>();
+
+    //constructor
+    public FibonacciSequence(){
+        fibonacci.add("0");
+        fibonacci.add("1");
+    }
+
+    //getter for list
+    public ArrayList<String> getFibonacci() {
+        return fibonacci;
+    }
+
+    //calculating one number fibonacci and add to array
+    public String calculateFibonacciNumber(){
+        BigInteger temp = new BigInteger(fibonacci.get(fibonacci.size()-1));
+        return temp.add(new BigInteger(fibonacci.get(fibonacci.size()-2))).toString();
+    }
+
+    //method for adding num to list
+    public void addFibonacciNumber(String S){
+        fibonacci.add(S);
+    }
+
+    //Fibonacci sequence by count of numbers
+    public void FibonacciNumberToLength(int lengthOfNumber) {
+        while (fibonacci.get(fibonacci.size()-1).length()<=lengthOfNumber){
+            addFibonacciNumber(calculateFibonacciNumber());
+        }
+    }
+
+    //Fibonacci sequence by min and max values
+    public void FibonacciNumberToMax(BigInteger max) {
+        while (max.compareTo(new BigInteger(fibonacci.get(fibonacci.size()-1)))>=0){
+            addFibonacciNumber(calculateFibonacciNumber());
+        }
+    }
+
+}
