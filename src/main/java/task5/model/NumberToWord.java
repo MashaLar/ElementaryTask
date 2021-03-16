1e32e0189b7e330e6603997530f0b62a99fd4e8e
MashaLar <mashalar01@gmail.com>
2021-03-16 19:04:31 +0200

    Refactored NumberToWord Added method for "одна" and "две"

--- src/main/java/task5/model/NumberToWord.java
+++ src/main/java/task5/model/NumberToWord.java
@@ -5,7 +5,7 @@ public class NumberToWord {
     //TODO: make output string variable
     // divide to several classes
 
-    final public String[][] words ={{"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"},
+    private static final String[][] WORDS ={{"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"},
             {"", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"},
             {"", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"},
             {"", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"},
@@ -17,41 +17,20 @@ public class NumberToWord {
         return new StringBuilder(str).reverse().toString();
     }
 
-    public void translate(String S){
+    public String translate(String S){
         if((S.matches("0+"))) {
-            System.out.println("ноль");
-            return;
+            return "ноль";
         }
-        String[] num = divideNumber(S);
+        StringBuilder result = new StringBuilder();
+        String[] num = divideNumber(S.replaceAll("[,;\\s]",""));
         for (int i = num.length-1; i>=0; i--) {
-            translateToWord(num[i]);
-            if(i>=1) endWordAdd(num[i],i);
+            result.append(translateToWord(num[i]));
+            if (i >= 1) {
+                if(i+3==4) result = hundredsUniqueNums(result);
+                result.append(endWordAdd(num[i], i));
+            }
         }
-    }
-
-    //TODO: made return?
-    private void translateToWord(String S){
-        if(S.length() ==1){
-            System.out.print(words[0][Integer.parseInt(S)]+" ");
-        }
-        else if(S.length() ==2){
-            twoNumbersToWord(S);
-        }
-        else if (S.length()==3){
-            System.out.print(words[3][Integer.parseInt(""+S.charAt(0))]+" ");
-            twoNumbersToWord(S.substring(1));
-        }
-
-    }
-
-    private void twoNumbersToWord(String S){
-        if(Integer.parseInt(S) >10 && Integer.parseInt(S)<20 )
-        {
-            System.out.print(words[1][Integer.parseInt(""+S.charAt(1))]+" ");
-        }
-        else {
-            System.out.print(words[2][Integer.parseInt(""+S.charAt(0))]+" "+words[0][Integer.parseInt(""+S.charAt(1))]+" ");
-        }
+        return result.toString();
     }
 
     private String[] divideNumber(String S){
@@ -63,22 +42,58 @@ public class NumberToWord {
         return devNum;
     }
 
-    //TODO: add "одна" and "две"
-    private void endWordAdd(String S, int row){
+    private String translateToWord(String S){
+        if(S.length() ==1){
+            return WORDS[0][Integer.parseInt(S)]+" ";
+        }
+        else if(S.length() ==2){
+            return twoNumbersToWord(S);
+        }
+        else if (S.length()==3){
+            return WORDS[3][Integer.parseInt(""+S.charAt(0))]+" "+ twoNumbersToWord(S.substring(1));
+        }
+        else{
+            return "Too big number";
+        }
+
+    }
+
+    private String twoNumbersToWord(String S){
+        if(Integer.parseInt(S) >10 && Integer.parseInt(S)<20 )
+        {
+            return WORDS[1][Integer.parseInt(""+S.charAt(1))]+" ";
+        }
+        else {
+            return WORDS[2][Integer.parseInt(""+S.charAt(0))]+" "+ WORDS[0][Integer.parseInt(""+S.charAt(1))]+" ";
+        }
+    }
+
+    private StringBuilder hundredsUniqueNums(StringBuilder S){
+        if(S.toString().matches(".*один $")){
+            S.delete(S.length()-6,S.length()-1);
+            S.append("одна ");
+        }
+        else if(S.toString().matches(".*два $")){
+            S.delete(S.length()-5,S.length()-1);
+            S.append("две ");
+        }
+        return S;
+    }
+
+    private String endWordAdd(String S, int row){
         int lastSymbol = Integer.parseInt(""+S.substring((S.length()-1)));
         int twoLastSymbols = 0;
         if(S.length()>=2) {
             twoLastSymbols = Integer.parseInt(S.substring((S.length()-2)));
         }
         if(lastSymbol == 0 || lastSymbol>=5 || (twoLastSymbols>10 && twoLastSymbols<20) ){
-            System.out.print(words[row+3][2]+" ");
+           return WORDS[row+3][2]+" ";
         }
         else if(lastSymbol == 1){
-            System.out.print(words[row+3][0]+" ");
+            return WORDS[row+3][0]+" ";
         }
         else {
-            System.out.print(words[row+3][1]+" ");
+            return WORDS[row+3][1]+" ";
         }
     }
-
 }
