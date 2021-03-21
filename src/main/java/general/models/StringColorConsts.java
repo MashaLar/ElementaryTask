8394d021541c6406e2a3b26968113c083b49f074
MashaLar <mashalar01@gmail.com>
2021-03-21 23:40:47 +0200

    Created interface for color constants

---
+++ src/main/java/general/models/StringColorConsts.java
@@ -0,0 +1,10 @@
+package general.models;
+
+public interface StringColorConsts {
+    String ANSI_RESET = "\u001B[0m";
+    String ANSI_RED_BOLD = "\u001B[31;1m";
+    String ANSI_YELLOW = "\u001B[38;5;220m";
+    String ANSI_BLUE = "\u001B[38;5;39m";
+    String ANSI_CYAN_CURSIVE = "\u001B[38;5;48;3m";
+    String ANSI_WHITE_BOLD = "\u001B[0;1m";
+}
